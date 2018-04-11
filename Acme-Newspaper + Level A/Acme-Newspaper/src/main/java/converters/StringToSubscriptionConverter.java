aac4bb9550cb8c67da79d6abbab4e574926ccf4b
joancafom <joanca.carmona597@gmail.com>
2018-04-11 23:04:03 +0200

    SubscriptionConverters 🌻
    - Contiene errores, pues ni el domain ni los repos estaban hechos. Se
    solucionaría con un simple import

---
+++ Acme-Newspaper + Level A/Acme-Newspaper/src/main/java/converters/StringToSubscriptionConverter.java
@@ -0,0 +1,36 @@
+
+package converters;
+
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Transactional
+public class StringToSubscriptionConverter implements Converter<String, Subscription> {
+
+	@Autowired
+	SubscriptionRepository	subscriptionRepository;
+
+
+	@Override
+	public Subscription convert(final String text) {
+		final Subscription result;
+		final int id;
+
+		try {
+			if (StringUtils.isEmpty(text))
+				result = null;
+			else {
+				id = Integer.valueOf(text);
+				result = this.subscriptionRepository.findOne(id);
+			}
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+		return result;
+	}
+}
